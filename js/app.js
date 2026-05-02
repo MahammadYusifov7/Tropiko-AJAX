7e38483fc30d5dbe93d699feae0f1e157dcb09c1
MahammadYusifov <ysfmaqa7@gmail.com>
2026-05-02 15:42:57 +0400

    commit2

--- js/app.js
+++ js/app.js
@@ -5,25 +5,49 @@ function menucontrol() {
 }
 
 let fixmenu = document.getElementById('fixmenu')
-window.addEventListener("scroll",function() {
-    if (window.scrollY >  window.innerHeight * 0.1) {
+window.addEventListener("scroll", function () {
+    if (window.scrollY > window.innerHeight * 0.1) {
         fixmenu.classList.add('menubarfix')
     } else {
         fixmenu.classList.remove('menubarfix')
     }
 })
 
+const content = document.getElementById('content')
+const ajax = new XMLHttpRequest()
+loadPage(window.location.hash.slice(1) || 'home')
+
+function loadPage(page) {
+    let file = `pages/${page}.htm`
+    ajax.open('GET', file)
+    ajax.send()
+
+    ajax.onload = function () {
+        content.innerHTML = this.response
+
+        // 🔥 BURA ƏLAVƏ ET
+        const lang = localStorage.getItem('lang') || 'en'
+        changeLang(lang)
+    }
+}
+
 let translations = {}
 
 async function loadTranslations() {
-    const response = await fetch('lang.json')
-    translations = await response.json();
-    const movcuddil = localStorage.getItem(('lang') || 'az');
-    changeLang(movcuddil)
+    try {
+        const response = await fetch('lang.json')
+        translations = await response.json();
+
+        const movcuddil = localStorage.getItem('lang') || 'az';
+        changeLang(movcuddil)
+    } catch (err) {
+        console.error('Translation yüklənmədi:', err)
+    }
 }
 
 function changeLang(lang) {
     const elements = document.querySelectorAll('[data-key]');
+
     elements.forEach(item => {
         const key = item.getAttribute('data-key')
 
@@ -35,4 +59,6 @@ function changeLang(lang) {
     localStorage.setItem('lang', lang);
     document.documentElement.lang = lang
 }
-loadTranslations()
+
+// səhifə tam yüklənəndən sonra çağır
+document.addEventListener('DOMContentLoaded', loadTranslations)
